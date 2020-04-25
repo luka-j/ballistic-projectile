f257aec4a446f3b89c73a48bfdce981c4e9ecd24
Luka Jovicic <luka.jovicic16@gmail.com>
2020-04-26 01:33:34 +0200

    Minor main refactor

--- projectile/main.py
+++ projectile/main.py
@@ -5,10 +5,10 @@ from projectile.Environment import Environment
 from projectile.Position import Position
 from projectile.Projectile import Projectile
 
+DT = 10**-2
 
-def launch_projectile(projectile: Projectile, pitch: float, yaw: float, velocity: float,
-                      outfile: TextIO, dt: float):
-    projectile.launch_at_angle(pitch, yaw, velocity)
+
+def fly_projectile(projectile: Projectile, outfile: TextIO, dt: float):
     while True:
         projectile.advance(dt)
         projectile.write_position(outfile)
@@ -19,7 +19,8 @@ def launch_projectile(projectile: Projectile, pitch: float, yaw: float, velocity
 if __name__ == '__main__':
     env = Environment()
     projectile = env.create_projectile(lambda t: 1, Position(math.radians(44.869389), math.radians(20.640221), 0))
+    projectile.launch_at_angle(math.pi / 4, math.pi/8, 10.0)
     output = open("/home/luka/Documents/mehanika-seminarski/test.out", "w")
-    launch_projectile(projectile, math.pi / 4, math.pi/8, 10.0, output, 10**-2)
+    fly_projectile(projectile, output, DT)
     output.close()
     print("Finished!")
