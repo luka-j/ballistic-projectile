60e1413e3673901bf086a7a5e42ac975f42c7b6b
Luka Jovicic <luka.jovicic16@gmail.com>
2020-04-25 23:29:47 +0200

    Fix Drag - it's not the same on all coordinates!

--- projectile/forces/DragForce.py
+++ projectile/forces/DragForce.py
@@ -5,14 +5,14 @@ from projectile.forces.Force import Force
 
 
 # noinspection PyUnresolvedReferences
-def intensity(projectile: Projectile, environment: Environment):
-    return 0.5 * environment.density(projectile.position.alt) * projectile.velocity() ** 2 * \
+def intensity(projectile: Projectile, environment: Environment, velocity: float):
+    return 0.5 * environment.density(projectile.position.alt) * velocity ** 2 * \
            projectile.cross_section() * projectile.drag_coef()
 
 
 class DragForce(Force):
     def __init__(self):
         self.intensity = [0, 0, 0]
-        super().__init__(lambda p, env: -p.directions[0] * intensity(p, env),
-                         lambda p, env: -p.directions[1] * intensity(p, env),
-                         lambda p, env: -p.directions[2] * intensity(p, env))
+        super().__init__(lambda p, env: -p.directions[0] * intensity(p, env, p.velocities[0]),
+                         lambda p, env: -p.directions[1] * intensity(p, env, p.velocities[1]),
+                         lambda p, env: -p.directions[2] * intensity(p, env, p.velocities[2]))
