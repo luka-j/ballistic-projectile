46fa581f7d7035af7219cdd907a803b94e83b3ef
Luka Jovicic <luka.jovicic16@gmail.com>
2020-05-01 19:54:21 +0200

    Fix position rounding error

--- projectile/Position.py
+++ projectile/Position.py
@@ -1,5 +1,11 @@
+import numpy as np
+
+
 class Position:
     def __init__(self, lat, lon, alt):
-        self.lat = lat
-        self.lon = lon
-        self.alt = alt
+        # this is CRUCIAL - forces such as Coriolis generate pretty small movement which is lost as a rounding error
+        # when doing operations with position
+        data = np.array([lat, lon, alt], "float128")
+        self.lat = data[0]
+        self.lon = data[1]
+        self.alt = data[2]
