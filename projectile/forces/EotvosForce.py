44de5a9edf5eb3105b305031a1a86c69e4a06945
Luka Jovicic <luka.jovicic16@gmail.com>
2020-05-03 01:20:53 +0200

    Fix Eotvos force

--- projectile/forces/EotvosForce.py
+++ projectile/forces/EotvosForce.py
@@ -1,10 +1,12 @@
 from math import cos
 
 from projectile.forces.Force import Force
+from projectile.Constants import X_INDEX
 
 
 class EotvosForce(Force):
     def __init__(self):
         super().__init__(lambda pr, env: 0,
                          lambda pr, env: 0,
-                         lambda pr, env: -2*env.earth_angular_velocity*pr.total_velocity*cos(pr.position.lat))
+                         lambda pr, env: 2 * pr.directions[X_INDEX] * env.earth_angular_velocity *
+                                         pr.total_velocity * cos(pr.position.lat))
--- projectile/main.py
+++ projectile/main.py
@@ -43,10 +43,10 @@ def fuel_flow(t: float):
 
 if __name__ == '__main__':
     env = Environment(surface_altitude=lambda p: 77)
-    projectile = env.create_projectile(10000, Position(math.radians(43), math.radians(22), 77),
+    projectile = env.create_projectile(10000, Position(math.radians(82), math.radians(172), 77),
                                        lambda axis, pitch, yaw: 20)
-    projectile.launch_at_angle(math.pi / 3, math.pi, 0)
-    projectile.add_thrust(ThrustForce(2800, fuel_flow, 70, 300000, 20))
+    projectile.launch_at_angle(math.pi / 3, math.pi/2, 0)
+    projectile.add_thrust(ThrustForce(2800, fuel_flow, 70, 200000, 20))
     fly_projectile(projectile, "/home/luka/Documents/mehanika-seminarski/test.csv", DT)
 
 #    if DEBUG:
