d25d8b7e423a60948339e88284796f42693bd31c
Luka Jovicic <luka.jovicic16@gmail.com>
2020-05-03 19:05:56 +0200

    Refactor atmosphere constants

--- projectile/Constants.py
+++ projectile/Constants.py
@@ -18,3 +18,61 @@ class DragCoefficients:
     SHORT_CYLINDER = 1.15
     STREAMLINED_BODY = 0.04
     STREAMLINED_HALFBODY = 0.09
+
+
+class Atmosphere:
+    def temp_lapse_rate(self, h: float):
+        raise ValueError("Undefined atmosphere!")
+
+    def mass_density(self, h: float):
+        raise ValueError("Undefined atmosphere!")
+
+    def base_temp(self, h: float):
+        raise ValueError("Undefined atmosphere!")
+
+    def atmosphere_layer_start(self, h: float):
+        raise ValueError("Undefined atmosphere!")
+
+    def molar_mass(self, h: float):
+        raise ValueError("Undefined atmosphere!")
+
+
+class StandardAtmosphere(Atmosphere):
+    def temp_lapse_rate(self, h: float):
+        if h < 11000: return -0.0065
+        if h < 20000: return 0
+        if h < 32000: return 0.001
+        if h < 47000: return 0.0028
+        if h < 51000: return 0
+        if h < 71000: return -0.0028
+        return -0.002
+
+    def mass_density(self, h: float):
+        if h < 11000: return 1.2250
+        if h < 20000: return 0.36391
+        if h < 32000: return 0.08803
+        if h < 47000: return 0.01322
+        if h < 51000: return 0.00143
+        if h < 71000: return 0.00086
+        return 0.000064
+
+    def base_temp(self, h: float):
+        if h < 11000: return 288.15
+        if h < 20000: return 216.65
+        if h < 32000: return 216.65
+        if h < 47000: return 228.65
+        if h < 51000: return 270.65
+        if h < 71000: return 270.65
+        return 214.65
+
+    def atmosphere_layer_start(self, h: float):
+        if h < 11000: return 0
+        if h < 20000: return 11000
+        if h < 32000: return 20000
+        if h < 47000: return 32000
+        if h < 51000: return 47000
+        if h < 71000: return 51000
+        return 71000
+
+    def molar_mass(self, h: float):
+        return 0.0289644  # this is a lie above 100km
--- projectile/Environment.py
+++ projectile/Environment.py
@@ -8,69 +8,20 @@ from projectile.forces.EotvosForce import EotvosForce
 from projectile.forces.Force import Force
 from projectile.forces.NewtonianGravity import NewtonianGravity
 from projectile.Projectile import Projectile
-from projectile.Constants import X_INDEX, Y_INDEX, Z_INDEX, DEBUG, R
+from projectile.Constants import X_INDEX, Y_INDEX, Z_INDEX, DEBUG, R, StandardAtmosphere
 from math import exp
 import numpy as np
-
-
-def std_temp_lapse_rate(h: float):
-    if h < 11000: return -0.0065
-    if h < 20000: return 0
-    if h < 32000: return 0.001
-    if h < 47000: return 0.0028
-    if h < 51000: return 0
-    if h < 71000: return -0.0028
-    return -0.002
-
-
-def std_mass_density(h: float):
-    if h < 11000: return 1.2250
-    if h < 20000: return 0.36391
-    if h < 32000: return 0.08803
-    if h < 47000: return 0.01322
-    if h < 51000: return 0.00143
-    if h < 71000: return 0.00086
-    return 0.000064
-
-
-def std_temp(h: float):
-    if h < 11000: return 288.15
-    if h < 20000: return 216.65
-    if h < 32000: return 216.65
-    if h < 47000: return 228.65
-    if h < 51000: return 270.65
-    if h < 71000: return 270.65
-    return 214.65
-
-
-def std_atmosphere_layer_start(h: float):
-    if h < 11000: return 0
-    if h < 20000: return 11000
-    if h < 32000: return 20000
-    if h < 47000: return 32000
-    if h < 51000: return 47000
-    if h < 71000: return 51000
-    return 71000
-
-
-def std_molar_mass(h: float):
-    return 0.0289644  # this is a lie above 100km
 
 
 class Environment:
 
     def __init__(self, earth_radius=6378137, earth_angular_velocity=7.2921159e-5, surface_altitude=lambda pos: 0,
-                 std_gravity_acc=9.80665, std_temp=std_temp, temp_lapse_rate=std_temp_lapse_rate,
-                 mass_density=std_mass_density, atmosphere_layers=std_atmosphere_layer_start, molar_mass=std_molar_mass):
+                 std_gravity_acc=9.80665, atmosphere=StandardAtmosphere()):
         self.earth_radius = earth_radius
         self.earth_angular_velocity = earth_angular_velocity
         self.surface_altitude = surface_altitude
         self.std_gravity = std_gravity_acc
-        self.std_temp = std_temp
-        self.temp_lapse_rate = temp_lapse_rate
-        self.mass_density = mass_density
-        self.atmosphere_layer_start = atmosphere_layers
-        self.molar_mass = molar_mass
+        self.atmosphere = atmosphere
         self.forces: List[Force] = [NewtonianGravity(), DragForce(), CoriolisForce(), EotvosForce(), CentrifugalForce()]
         self.total_forces_impact = np.zeros((len(self.forces), 3))
         # it'd be prettier if total_forces_impact was dict, but that kills performance
@@ -92,12 +43,12 @@ class Environment:
         if altitude > 100000:
             return 0
         h = altitude
-        rho_b = self.mass_density(h)
-        T = self.std_temp(h)
-        L = self.temp_lapse_rate(h)
+        rho_b = self.atmosphere.mass_density(h)
+        T = self.atmosphere.base_temp(h)
+        L = self.atmosphere.temp_lapse_rate(h)
         g_0 = self.std_gravity
-        h_b = self.atmosphere_layer_start(h)
-        M = self.molar_mass(h)
+        h_b = self.atmosphere.atmosphere_layer_start(h)
+        M = self.atmosphere.molar_mass(h)
         if L == 0:
             return rho_b * exp((-g_0 * M * (h - h_b)) / (R * T))
         else:
@@ -105,8 +56,9 @@ class Environment:
 
     def pressure(self, altitude: float) -> float:
         rho = self.density(altitude)
-        temp = self.std_temp(altitude) + (altitude-self.atmosphere_layer_start(altitude))*self.temp_lapse_rate(altitude)
-        return rho / self.molar_mass(altitude) * R * temp
+        temp = self.atmosphere.base_temp(altitude) + (altitude-self.atmosphere.atmosphere_layer_start(altitude)) \
+             * self.atmosphere.temp_lapse_rate(altitude)
+        return rho / self.atmosphere.molar_mass(altitude) * R * temp
 
     def get_forces_intensity(self, projectile) -> np.array:
         intensities = np.zeros(3, "float128")
