75c76da79581736bf4813027c5c419ef2966c7d7
Luka Jovicic <luka.jovicic16@gmail.com>
2020-05-19 11:12:30 +0200

    Fix centrifugal

--- projectile/forces/CentrifugalForce.py
+++ projectile/forces/CentrifugalForce.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
-from math import cos, sin, fabs
+from math import cos, sin
 
 from projectile.core.Projectile import Projectile
 from projectile.forces.Force import Force
-from projectile.util import sgn
 
 
 # noinspection PyUnresolvedReferences
@@ -19,5 +18,5 @@ class CentrifugalForce(Force):
     """
     def __init__(self):
         super().__init__(lambda pr, env: 0,
-                         lambda pr, env: intensity(pr, env) * (-sgn(pr.position.lat)) * cos(pr.position.lat),
-                         lambda pr, env: intensity(pr, env) * fabs(sin(pr.position.lat)))
+                         lambda pr, env: intensity(pr, env) * sin(pr.position.lat),
+                         lambda pr, env: intensity(pr, env) * cos(pr.position.lat))
